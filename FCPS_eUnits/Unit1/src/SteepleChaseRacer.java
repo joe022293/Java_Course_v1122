db4e174ab459e678218e01279e7f5a465a81aa01
joe022293 <joe.022293@gmail.com>
2025-02-27 10:30:09 +0800

    lab08

---
+++ FCPS_eUnits/Unit1/src/SteepleChaseRacer.java
@@ -0,0 +1,23 @@
+public class SteepleChaseRacer extends Racer
+{
+    public SteepleChaseRacer(int y)
+    {
+        super(y);
+    }
+    public void jumpRight()
+    {
+        turnLeft();
+        while(!rightIsClear())
+        {
+            move();
+        }
+        turnRight();
+        move();
+        turnRight();
+        while (frontIsClear())
+        { 
+            move();
+        }
+        turnLeft();
+    }
+}
