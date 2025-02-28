4c9c616ef6edbb04565ae40a2c24a8a7ad1efc93
joe022293 <joe.022293@gmail.com>
2025-02-28 23:12:42 +0800

    Lab16
    without private

--- FCPS_eUnits/Unit1/src/Shifter.java
+++ FCPS_eUnits/Unit1/src/Shifter.java
@@ -9,9 +9,52 @@
          super(1, y, Display.EAST, 0);
       
       }
-   	/*********************************
-   	 *                               *
-   	 * What methods must you define? *
-   	 *                               *
-   	 *********************************/
+      public void run()
+      {
+         sh();
+      }
+      public void sh() 
+      {
+         for(int i=0;i<7;i++)
+         {
+            move();
+         }
+         turnAround();
+         while (frontIsClear() | nextToABeeper())
+         {
+            if(nextToABeeper())
+            {
+               shiftBeepers();
+               turnAround();
+            }
+            move();
+        }
+      }
+      public  void turnAround ()
+      {
+        turnLeft();
+        turnLeft();
+      }
+      public  void shiftBeepers ()
+      {
+        while (nextToABeeper()) 
+        { 
+            pickBeeper();    
+        }
+        turnAround();
+        move();
+        while (hasBeepers()) 
+        { 
+            putBeeper();    
+        }
+      }
+      @Override
+      public void workCorner() {
+          // Implement workCorner method
+      }
+  
+      @Override
+      public void moveOneBlock() {
+          // Implement moveOneBlock method
+      }
    }
