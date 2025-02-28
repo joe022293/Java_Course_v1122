69ae5690f2432d469f8f22d8f08f4c276711b817
joe022293 <joe.022293@gmail.com>
2025-02-28 14:51:56 +0800

    Lab10

---
+++ FCPS_eUnits/Unit1/src/Lab10.java
@@ -0,0 +1,27 @@
+import edu.fcps.karel2.Display;
+public class Lab10 {
+    public static void followWallsRight (Athlete arg)
+    {
+        while (!arg.nextToABeeper()) 
+        { 
+            if(!arg.rightIsClear() & arg.frontIsClear())
+            {
+                arg.move();
+            }
+            else if(!arg.rightIsClear() & !arg.frontIsClear())
+            {
+                arg.turnLeft();
+            }
+            else if(arg.rightIsClear())
+            {
+                arg.turnRight();
+                arg.move();
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Display.openWorld("maps/maze3.map");
+        Athlete p = new Athlete(1,1,Display.NORTH,Display.INFINITY);
+        Lab10.followWallsRight(p);
+    }
+}
